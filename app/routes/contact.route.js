1710a7a96ce1ac1778ec4e64445118a073a849cf
LePhuocLoi4850 <122241647+LePhuocLoi4850@users.noreply.github.com>
2023-10-04 08:22:22 +0700

    Cai dat cac handler truy xuat CSDL

--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -6,10 +6,10 @@ const router = express.Router();
 router.route("/")
     .get(contacts.findALL)
     .post(contacts.create)
-    .delete(contacts.deleteALL);
+    .delete(contacts.deleteAll);
 
 router.route("/favorite")
-    .get(contacts.findALLFavorite);
+    .get(contacts.findAllFavorite);
 
 router.route("/:id")
     .get(contacts.findOne)
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const app = require("./app");
+const MongoDB = require("./app/utils/mongodb.util");
 const config = require("./app/config");
 
-const PORT = config.app.port;
 
-app.listen(PORT, () => {
-    console.log('Server is running on port ${PORT}.');
-});
+async function startServer(){
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log("connected to the database!");
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`server is running on port ${PORT}`);
+        });
+    } catch (error){
+        console.log("Cannot connect to the database!", error);
+        process.exit(1);
+    }
+};
+startServer();
